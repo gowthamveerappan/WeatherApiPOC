ee3f17874f3c0814d19a18f6567576140b0c9a50
unknown <gowthamveerappan@gmail.com>
2019-02-24 20:06:09 +0530

    Checking for permission

--- app/src/main/java/com/weather/weathersamplepoc/MainActivity.java
+++ app/src/main/java/com/weather/weathersamplepoc/MainActivity.java
@@ -1,6 +1,10 @@
 package com.weather.weathersamplepoc;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -9,6 +13,8 @@ import com.weather.weathersamplepoc.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int MY_LOCATION_REQUEST_CODE =1111;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +22,37 @@ public class MainActivity extends AppCompatActivity {
 
         //Checking Network connectivity
         if(NetworkUtils.isNetworkConnected(this) == true){
+            if(checkLocationPermission() == true){
 
+            }
         }else{
             Toast.makeText(this,R.string.offline_text,Toast.LENGTH_SHORT).show();
             this.finish();
+        }
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                this.finish();
+            }
         }
     }
 }
